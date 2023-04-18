e198bdcbcc7aedbdc558d1eeb8d095f5681b52d6
陈微（Chen Wei）(chenwei02) <chenwei02@dreamxr.com>
2023-04-18 15:13:15 +0800

    test

--- packages/chenwei02/lib/index.d.ts
+++ packages/chenwei02/lib/index.d.ts
@@ -1,6 +1,6 @@
-export * from './src/index';
-import { App } from 'vue';
+import { App } from 'vue'
+export * from './src/index'
 declare const _default: {
-    install: (app: App) => void;
-};
-export default _default;
+  install: (app: App) => void
+}
+export default _default
