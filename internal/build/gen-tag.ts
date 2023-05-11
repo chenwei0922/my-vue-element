04fbb2b4da180793cb41b49c4ef80bf55efe1f2e
陈微（Chen Wei）(chenwei02) <chenwei02@dreamxr.com>
2023-05-11 16:13:28 +0800

    debug

---
+++ internal/build/gen-tag.ts
@@ -0,0 +1,20 @@
+import { run, qyOutput } from '@chenwei02/build-utils'
+import { series } from 'gulp'
+import path from 'node:path'
+import fs from 'fs'
+
+export const setTag = async () => {
+  const srcPath = path.join(qyOutput, '/package.json')
+  const content = fs.readFileSync(srcPath, 'utf8')
+  const regex = /"version": ["|'](.*)["|']/i
+  if (regex.test(content)) {
+    const version = content.match(regex)?.[1]
+    console.log('find it', version)
+    run(`git tag ${version}`)
+    run(`git push --tags`)
+  } else {
+    console.log('not find it')
+  }
+}
+
+export default series(setTag)
